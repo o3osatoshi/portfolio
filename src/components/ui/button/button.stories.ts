21ab77b1cede0401ab5c3f9cea56c5290f6ab710
o3osatoshi <satoshi.ogura@engr.work>
2025-04-11 13:38:16 +0900

    style(ui): rename

--- src/components/ui/button/button.stories.ts
+++ src/components/ui/button/button.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { Button } from "@/components/ui/Button/button";
+import { Button } from "@/components/ui/button/button";
 
 const meta = {
   title: "shadcn/Button",
