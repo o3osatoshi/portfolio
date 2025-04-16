df7cf0a220d8c2dded53196562166f11dece9091
o3osatoshi <satoshi.ogura@engr.work>
2025-04-16 10:52:39 +0900

    fix(auth): rm unnecessary env

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -1,26 +1,9 @@
 import type { NextAuthConfig } from "next-auth";
 import Google from "next-auth/providers/google";
 
-const clientId = process.env.AUTH_GOOGLE_ID;
-const clientSecret = process.env.AUTH_GOOGLE_SECRET;
-const secret = process.env.AUTH_SECRET;
-
-if (
-  clientId === undefined ||
-  clientSecret === undefined ||
-  secret === undefined
-)
-  throw new Error("missing required env params");
-
 // https://authjs.dev/guides/edge-compatibility
 export default {
-  providers: [
-    Google({
-      clientId,
-      clientSecret,
-    }),
-  ],
-  secret,
+  providers: [Google],
   callbacks: {
     async jwt({ token, user }) {
       if (user !== undefined) {
