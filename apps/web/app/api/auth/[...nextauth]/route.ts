891d35ba3ec60e2d91dd2855614dd61d5054050a
o3osatoshi <satoshi.ogura@engr.work>
2025-04-25 10:18:42 +0900

    fix(web): api auth

--- apps/web/app/api/auth/[...nextauth]/route.ts
+++ apps/web/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,2 @@
-import { handlers } from "@repo/auth";
+import { handlers } from "@/lib/auth";
 export const { GET, POST } = handlers;
---
+++ apps/web/lib/auth.ts
@@ -0,0 +1,14 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { authConfig } from "@repo/auth/config";
+import { prisma } from "@repo/db";
+import NextAuth from "next-auth";
+
+const nextAuth = NextAuth({
+  // @ts-ignore
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  ...authConfig,
+});
+
+export const handlers = nextAuth.handlers;
+export const auth: typeof nextAuth.auth = nextAuth.auth;
