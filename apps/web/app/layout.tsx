a46aa8a084425a60eb78e2d8effb6430a25ac346
o3osatoshi <satoshi.ogura@engr.work>
2025-04-26 18:47:44 +0900

    feat(web): apply google font

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,13 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+
 import "@repo/ui/globals.css";
+
+const fontSans = Geist({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+const fontMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+});
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
+      >
+        {children}
+      </body>
     </html>
   );
 }
