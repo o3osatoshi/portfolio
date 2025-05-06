070e7adcb8d1ff9916b79a50a1b78faecfa44a59
o3osatoshi <satoshi.ogura@engr.work>
2025-05-06 16:54:49 +0900

    build(web): add monorepo prisma plugin

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,8 +1,16 @@
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@repo/ui"],
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins.push(new PrismaPlugin());
+    }
+    return config;
   },
 };
 
