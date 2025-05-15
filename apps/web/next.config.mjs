a937340d64fcd52e24ca4eb6f3e7333e0c05fa16
o3osatoshi <satoshi.ogura@engr.work>
2025-05-15 11:43:18 +0900

    fix(web): prisma

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,8 +1,14 @@
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@repo/ui"],
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) config.plugins = [...config.plugins, new PrismaPlugin()];
+    return config;
   },
 };
 
