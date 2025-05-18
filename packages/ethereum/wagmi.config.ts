48abfbec01a9358914462fc0b79a092f67557b87
o3osatoshi <satoshi.ogura@engr.work>
2025-05-18 11:55:56 +0900

    feat(ethereum): add wagmi config

---
+++ packages/ethereum/wagmi.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from "@wagmi/cli";
+import { etherscan, react } from "@wagmi/cli/plugins";
+import { erc20Abi } from "viem";
+import { mainnet } from "wagmi/chains";
+
+const apiKey = process.env.ETHERSCAN_API_KEY;
+if (apiKey === undefined) {
+  throw new Error("ETHERSCAN_API_KEY is not set");
+}
+
+export default defineConfig({
+  out: "src/generated.ts",
+  contracts: [
+    {
+      name: "erc20",
+      abi: erc20Abi,
+    },
+  ],
+  plugins: [
+    etherscan({
+      apiKey,
+      chainId: mainnet.id,
+      contracts: [
+        {
+          name: "WETH",
+          address: {
+            [mainnet.id]: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+          },
+        },
+      ],
+    }),
+    react(),
+  ],
+});
