ad3729120f7001f1e0e079ce0e76d5b59b5fc697
o3osatoshi <satoshi.ogura@engr.work>
2025-08-07 20:23:25 +0900

    refactor(functions): mv to apps

--- apps/functions/src/index.ts
+++ apps/functions/src/index.ts
@@ -14,6 +14,6 @@ import { onRequest } from "firebase-functions/v2/https";
 // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
+  logger.info("Hello logs! under the apps!", { structuredData: true });
+  response.send("Hello from Firebase! under the apps!");
 });
