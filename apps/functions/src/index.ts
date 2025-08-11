801bc50694488fa79c3be1d5a37481eb218b37d3
o3osatoshi <satoshi.ogura@engr.work>
2025-08-11 16:52:51 +0900

    feat(functions): import domain

--- apps/functions/src/index.ts
+++ apps/functions/src/index.ts
@@ -9,11 +9,17 @@
 
 import * as logger from "firebase-functions/logger";
 import { onRequest } from "firebase-functions/v2/https";
+import { logSampleMessage, logCustomMessage } from "@repo/domain";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = onRequest((request, response) => {
   logger.info("Hello logs! by turbo!", { structuredData: true });
+  
+  // Use domain package functions
+  logSampleMessage();
+  logCustomMessage("Firebase Functions is working with domain package!");
+  
   response.send("Hello from Firebase! by turbo!");
 });
