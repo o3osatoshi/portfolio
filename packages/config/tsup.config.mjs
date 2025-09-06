58f3b4eeb46a0c8ea09c12c0bd7838d37d87bea8
o3osatoshi <satoshi.ogura@engr.work>
2025-09-06 14:51:28 +0900

    chore(config): rm vitest

--- packages/config/tsup.config.mjs
+++ packages/config/tsup.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig([
-  // tsup preset
   {
     entry: { index: "src/tsup/index.ts" },
     format: ["esm"],
@@ -14,19 +13,5 @@ export default defineConfig([
     target: "es2022",
     platform: "node",
     outDir: "dist/tsup",
-  },
-  // vitest base config
-  {
-    entry: { base: "src/vitest/base.ts" },
-    format: ["esm"],
-    dts: false,
-    sourcemap: false,
-    clean: false,
-    treeshake: true,
-    minify: false,
-    splitting: false,
-    target: "es2022",
-    platform: "node",
-    outDir: "dist/vitest",
   },
 ]);
