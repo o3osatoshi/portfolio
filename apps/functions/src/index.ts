c6efb1f1dfda505f8c970558c1bffffcfb12a634
o3osatoshi <satoshi.ogura@engr.work>
2025-09-26 20:56:00 +0900

    feat(funcs): add application example

--- apps/functions/src/index.ts
+++ apps/functions/src/index.ts
@@ -7,14 +7,61 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
+import {
+  GetTransactionsUseCase,
+  parseGetTransactionsRequest,
+} from "@repo/application";
+import { PrismaTransactionRepository } from "@repo/prisma";
 import * as logger from "firebase-functions/logger";
 import { onRequest } from "firebase-functions/v2/https";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = onRequest((_request, response) => {
-  logger.info("Hello logs! by turbo!", { structuredData: true });
+const repo = new PrismaTransactionRepository();
+const usecase = new GetTransactionsUseCase(repo);
 
-  response.send("Hello from Firebase! by turbo! with clean architecture!");
-});
+export const helloWorld = onRequest(
+  { invoker: "public" },
+  (_request, response) => {
+    logger.info("Hello logs! by turbo!", { structuredData: true });
+
+    response.send("Hello from Firebase! by turbo! with clean architecture!");
+  },
+);
+
+export const getTransactions = onRequest(
+  { invoker: "public" },
+  async (request, response) => {
+    const userId = request.query["userId"];
+    const _userId =
+      typeof userId === "string"
+        ? userId
+        : Array.isArray(userId)
+          ? userId[0]
+          : undefined;
+    logger.info(
+      `[GET /getTransactions] called with userId=${_userId ?? "none"}`,
+      { structuredData: true },
+    );
+    if (_userId === undefined) {
+      response.status(400).send("Missing userId");
+      return;
+    }
+
+    const res = parseGetTransactionsRequest({ userId: _userId });
+    if (res.isErr()) {
+      response.status(400).send("Invalid request");
+      return;
+    }
+
+    const result = await usecase.execute(res.value);
+    if (result.isErr()) {
+      response.status(500).send(result.error.message);
+      return;
+    }
+    const transactions = result.value;
+
+    response.json(transactions);
+  },
+);
