9f3d3faaea66e071dc439d0fbe07262ed9996d1b
o3osatoshi <satoshi.ogura@engr.work>
2025-09-26 20:22:15 +0900

    refactor: rm prisma rust engine

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,15 +1,9 @@
-import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
   transpilePackages: ["@repo/prisma", "@repo/application", "@repo/domain"],
-  webpack: (config, { isServer }) => {
-    if (isServer) config.plugins = [...config.plugins, new PrismaPlugin()];
-    return config;
-  },
 };
 
 export default nextConfig;
