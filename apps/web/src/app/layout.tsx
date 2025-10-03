75264eb68e849b76116c6164db67975f1afa2a45
o3osatoshi <satoshi.ogura@engr.work>
2025-10-03 21:15:33 +0900

    feat(web): apply speed insights

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "@o3osatoshi/ui/globals.css";
 
 import { GoogleAnalytics } from "@next/third-parties/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 
 import { Providers } from "@/app/_components/providers";
@@ -51,6 +52,7 @@ export default function RootLayout({
       <body
         className={`${fontSans.variable} ${fontMono.variable} ${jetbrainsMono.variable} font-sans antialiased`}
       >
+        <SpeedInsights />
         <Providers>{children}</Providers>
       </body>
     </html>
