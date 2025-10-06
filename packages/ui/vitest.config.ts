39dec1925f60144e192a872657ee46203eded5dd
o3osatoshi <satoshi.ogura@engr.work>
2025-10-06 15:53:58 +0900

    chore(ui): add stories coverage exclude

--- packages/ui/vitest.config.ts
+++ packages/ui/vitest.config.ts
@@ -3,5 +3,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { browserTestPreset } from "@o3osatoshi/config/vitest";
 
 export default browserTestPreset({
+  coverage: {
+    exclude: ["**/*.stories.tsx"],
+  },
   plugins: [tsconfigPaths()],
 });
