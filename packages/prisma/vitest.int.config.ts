76488623c5b2720fc6ba697ed2f9e026bd5ee1ba
o3osatoshi <satoshi.ogura@engr.work>
2025-10-07 20:53:09 +0900

    refactor(prisma): handle integration test as unit

--- packages/prisma/vitest.int.config.ts
+++
@@ -1,9 +0,0 @@
-import { baseTestPreset } from "@o3osatoshi/config/vitest";
-
-export default baseTestPreset({
-  test: {
-    hookTimeout: 120_000,
-    include: ["src/**/*.int.spec.ts"],
-    testTimeout: 120_000,
-  },
-});
