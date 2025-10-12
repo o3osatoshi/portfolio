3d8562cbcd67e5549fea6eddce7a94dcda7e59cc
o3osatoshi <satoshi.ogura@engr.work>
2025-10-12 10:34:54 +0900

    chore(storybook): apply WCAG 2 AA

--- apps/storybook/.storybook/preview.ts
+++ apps/storybook/.storybook/preview.ts
@@ -4,6 +4,24 @@ import type { Preview } from "@storybook/react-vite";
 
 const preview: Preview = {
   parameters: {
+    a11y: {
+      config: {
+        rules: [
+          { id: "color-contrast", enabled: true },
+          { id: "landmark-one-main", enabled: true },
+          { id: "region", enabled: true },
+        ],
+      },
+      manual: false,
+      options: {
+        element: "#storybook-root",
+        restoreScroll: true,
+        runOnly: {
+          type: "tag",
+          values: ["wcag2a", "wcag2aa", "wcag21aa"],
+        },
+      },
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
