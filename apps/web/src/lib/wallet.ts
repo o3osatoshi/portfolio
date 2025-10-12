f19d48109b9c750b355f90c1a5c9b025e891a3ba
o3osatoshi <satoshi.ogura@engr.work>
2025-10-12 15:48:54 +0900

    fix(web-wallet): rename env

--- apps/web/src/lib/wallet.ts
+++ apps/web/src/lib/wallet.ts
@@ -2,9 +2,9 @@ import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { http } from "wagmi";
 import { holesky } from "wagmi/chains";
 
-const projectId = process.env["NEXT_PUBLIC_PROJECT_ID"];
+const projectId = process.env["NEXT_PUBLIC_RAINBOW_PROJECT_ID"];
 if (projectId === undefined) {
-  throw new Error("NEXT_PUBLIC_PROJECT_ID is undefined");
+  throw new Error("NEXT_PUBLIC_RAINBOW_PROJECT_ID is undefined");
 }
 
 export const config = getDefaultConfig({
