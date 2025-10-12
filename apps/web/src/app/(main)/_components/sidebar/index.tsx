24031b7ab98629ba23226694ae8b28c3f6f9ba05
o3osatoshi <satoshi.ogura@engr.work>
2025-10-12 17:00:23 +0900

    refactor(web-sidebar): extract service logo

--- apps/web/src/app/(main)/_components/sidebar/index.tsx
+++ apps/web/src/app/(main)/_components/sidebar/index.tsx
@@ -1,14 +1,12 @@
 import { FlaskConical, User } from "lucide-react";
 import type * as React from "react";
 
+import { ServiceLogo } from "@/app/(main)/_components/sidebar/service-logo";
 import { getLabel, getPathName } from "@/utils/handle-nav";
 import {
   SidebarContent,
   SidebarFooter,
   SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
   Sidebar as UiSidebar,
 } from "@o3osatoshi/ui/client";
 
@@ -16,7 +14,6 @@ import { NavMain } from "./nav-main";
 import { NavProjects } from "./nav-projects";
 import { NavSecondary } from "./nav-secondary";
 import { NavUser } from "./nav-user";
-import { SidebarLink } from "./sidebar-link";
 
 const data = {
   navMain: [
@@ -66,17 +63,7 @@ export function Sidebar({ ...props }: React.ComponentProps<typeof UiSidebar>) {
   return (
     <UiSidebar variant="inset" {...props}>
       <SidebarHeader>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <SidebarMenuButton asChild size="lg">
-              <SidebarLink href="/">
-                <span className="truncate font-code text-xl leading-tight md:text-2xl">
-                  o3osatoshi
-                </span>
-              </SidebarLink>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-        </SidebarMenu>
+        <ServiceLogo />
       </SidebarHeader>
       <SidebarContent>
         <NavMain items={data.navMain} />
