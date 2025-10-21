1e24427b42290a3d5710c3ca2825789117e02f99
o3osatoshi <satoshi.ogura@engr.work>
2025-10-21 22:13:03 +0900

    refactor(get-transactions): enhance error handling and improve response parsing logic

--- apps/web/src/services/get-transactions.ts
+++ apps/web/src/services/get-transactions.ts
@@ -1,10 +1,10 @@
-import type { ResultAsync } from "neverthrow";
+import { errAsync, type ResultAsync } from "neverthrow";
 
 import { getPath } from "@/utils/handle-nav";
 import { nextFetch } from "@/utils/next-fetch";
 import type { Transactions } from "@/utils/validation";
 import { transactionsSchema } from "@/utils/validation";
-import { parseWith } from "@o3osatoshi/toolkit";
+import { deserializeError, parseWith } from "@o3osatoshi/toolkit";
 
 interface Props {
   userId?: string | undefined;
@@ -18,10 +18,14 @@ export function getTransactions({
   return nextFetch({
     path: getPath("labs-transactions"),
     search,
-  }).andThen((res) =>
-    parseWith(transactionsSchema, {
+  }).andThen((res) => {
+    if (!res.ok) {
+      const err = deserializeError(res.body);
+      return errAsync(err);
+    }
+    return parseWith(transactionsSchema, {
       action: "ParseTransactionsResponse",
       layer: "UI",
-    })(res.body),
-  );
+    })(res.body);
+  });
 }
