0fcf68f4765ed3b10206b9700b4aa2c48bf7d25f
o3osatoshi <satoshi.ogura@engr.work>
2025-11-09 18:34:40 +0900

    refactor(functions): use Firebase secrets for `DATABASE_URL` in HTTP function

--- apps/functions/src/index.ts
+++ apps/functions/src/index.ts
@@ -3,13 +3,21 @@ import {
   createExpressRequestHandler,
 } from "@repo/interface/http/node";
 import { createPrismaClient, PrismaTransactionRepository } from "@repo/prisma";
+import { defineSecret } from "firebase-functions/params";
 import { onRequest } from "firebase-functions/v2/https";
 
 import { env } from "./env";
 
-const client = createPrismaClient({ connectionString: env.DATABASE_URL });
+const databaseUrl = defineSecret("DATABASE_URL");
+
+const client = createPrismaClient({
+  connectionString: databaseUrl.value() ?? env.DATABASE_URL,
+});
 const repo = new PrismaTransactionRepository(client);
 
 const app = buildApp({ transactionRepo: repo });
 
-export const api = onRequest(createExpressRequestHandler(app));
+export const api = onRequest(
+  { secrets: [databaseUrl] },
+  createExpressRequestHandler(app),
+);
