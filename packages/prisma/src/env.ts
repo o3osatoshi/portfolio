9f9420f7d9782d0d70bb0b6a56c0edb5aaeb4173
o3osatoshi <satoshi.ogura@engr.work>
2025-11-09 13:07:34 +0900

    refactor(env, ci): set default `DATABASE_URL` and remove `.env` export in workflow

--- packages/prisma/src/env.ts
+++ packages/prisma/src/env.ts
@@ -4,7 +4,7 @@ import { createEnv } from "@o3osatoshi/toolkit";
 
 export const env = createEnv(
   {
-    DATABASE_URL: z.string().min(1),
+    DATABASE_URL: z.string().min(1).default(""),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
