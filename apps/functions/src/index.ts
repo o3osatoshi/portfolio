b39e64aa13a29bf08a9181c736d943de5646db11
o3osatoshi <satoshi.ogura@engr.work>
2025-11-09 18:39:57 +0900

    refactor(functions): remove fallback to `env.DATABASE_URL` in Prisma client initialization

--- apps/functions/src/index.ts
+++ apps/functions/src/index.ts
@@ -6,13 +6,9 @@ import { createPrismaClient, PrismaTransactionRepository } from "@repo/prisma";
 import { defineSecret } from "firebase-functions/params";
 import { onRequest } from "firebase-functions/v2/https";
 
-import { env } from "./env";
-
 const databaseUrl = defineSecret("DATABASE_URL");
 
-const client = createPrismaClient({
-  connectionString: databaseUrl.value() ?? env.DATABASE_URL,
-});
+const client = createPrismaClient({ connectionString: databaseUrl.value() });
 const repo = new PrismaTransactionRepository(client);
 
 const app = buildApp({ transactionRepo: repo });
