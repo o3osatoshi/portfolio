b105419884a4a110f56572903e9915dc10283b87
o3osatoshi <satoshi.ogura@engr.work>
2025-12-02 22:09:16 +0900

    chore(web3-crud): comment out RainbowKit integration and simplify Web3Provider usage

--- apps/web/src/lib/wallet.ts
+++ apps/web/src/lib/wallet.ts
@@ -1,15 +1,15 @@
-import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { http } from "wagmi";
-import { holesky } from "wagmi/chains";
-
-import { env } from "@/env/client";
-
-export const config = getDefaultConfig({
-  appName: "RainbowKit demo",
-  chains: [holesky],
-  projectId: env.NEXT_PUBLIC_RAINBOW_PROJECT_ID,
-  ssr: true,
-  transports: {
-    [holesky.id]: http(),
-  },
-});
+// import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+// import { http } from "wagmi";
+// import { holesky } from "wagmi/chains";
+//
+// import { env } from "@/env/client";
+//
+// export const config = getDefaultConfig({
+//   appName: "RainbowKit demo",
+//   chains: [holesky],
+//   projectId: env.NEXT_PUBLIC_RAINBOW_PROJECT_ID,
+//   ssr: true,
+//   transports: {
+//     [holesky.id]: http(),
+//   },
+// });
