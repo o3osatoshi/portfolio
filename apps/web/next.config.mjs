edf0b3e48ae9647a2303145149834d67531e9e33
o3osatoshi <satoshi.ogura@engr.work>
2025-12-05 22:17:08 +0900

    chore(web): add `typegen` script and enable experimental `turbopackFileSystemCacheForDev` in Next.js configuration

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
   transpilePackages: [
     "@repo/interface",
     "@repo/prisma",
