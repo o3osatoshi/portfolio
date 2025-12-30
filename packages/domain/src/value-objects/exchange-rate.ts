b78cefc74e4d25329b2193f0f0b4098fe217a712
o3osatoshi <satoshi.ogura@engr.work>
2025-12-30 16:10:04 +0900

    fix(exchange-rate): remove unnecessary type annotation from Result import

--- packages/domain/src/value-objects/exchange-rate.ts
+++ packages/domain/src/value-objects/exchange-rate.ts
@@ -1,4 +1,4 @@
-import { err, ok, type Result } from "neverthrow";
+import { err, ok, Result } from "neverthrow";
 
 import { domainValidationError } from "../domain-error";
 import type { Brand } from "./brand";
