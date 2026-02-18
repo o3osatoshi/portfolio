b06e54e8cf843641e3d53f43728ad57dd42d1ae7
o3osatoshi <satoshi.ogura@engr.work>
2026-02-18 20:51:22 +0900

    refactor(interface): reuse auth CliPrincipal type

--- packages/interface/src/http/core/types.ts
+++ packages/interface/src/http/core/types.ts
@@ -1,4 +1,4 @@
-import type { AuthUser } from "@repo/auth";
+import type { AuthUser, CliPrincipal } from "@repo/auth";
 
 import type { Logger, RequestLogger } from "@o3osatoshi/logging";
 import type { RichError } from "@o3osatoshi/toolkit";
@@ -6,12 +6,7 @@ import type { RichError } from "@o3osatoshi/toolkit";
 export type ContextEnv = {
   Variables: {
     authUser?: AuthUser;
-    cliPrincipal?: {
-      issuer: string;
-      scopes: string[];
-      subject: string;
-      userId: string;
-    };
+    cliPrincipal?: CliPrincipal;
     error?: RichError;
     logger?: Logger;
     requestId?: string;
