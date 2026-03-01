7ead50069d5748aa45f6fd0ce94a2d0c019e21eb
o3osatoshi <satoshi.ogura@engr.work>
2026-03-01 20:36:19 +0900

    chore(cli): update default oidc client id

--- packages/cli/src/lib/default-runtime-config.ts
+++ packages/cli/src/lib/default-runtime-config.ts
@@ -3,7 +3,7 @@ import type { CliRuntimeConfig } from "./types";
 export const DEFAULT_RUNTIME_CONFIG: CliRuntimeConfig = {
   oidc: {
     audience: "https://api.o3o.app",
-    clientId: "fHkQMeu3w3HHjy4c1Jh7sossO6R7QY1V",
+    clientId: "NCWwZFxrbAu2TR1gJy2jPJNOqb7NInuf",
     issuer: "https://dev-yr26ks6rmc6zj8nj.us.auth0.com",
     redirectPort: 38080,
   },
